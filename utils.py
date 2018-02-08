c24213628839cec05631f0818d4a5881882d46bb
Sébastien Combéfis <seb478@gmail.com>
2018-02-08 11:51:16 +0100

    UtilsChange

--- utils.py
+++ utils.py
@@ -2,27 +2,30 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
-
+import math
 def fact(n):
-    """Computes the factorial of a natural number.
-    
-    Pre: -
-    Post: Returns the factorial of 'n'.
-    Throws: ValueError if n < 0
-    """
-
+         if n == 0:
+            return 1
+         else:
+            return n*fact(n - 1)
+        pass
 def roots(a, b, c):
-    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
-    
-    Pre: -
-    Post: Returns a tuple with zero, one or two elements corresponding
-          to the roots of the ax^2 + bx + c polynomial.
-    """
-    pass
+    delta = b^2 - (4*a*c)
+        if delta > 0:
+            root1 = (b + sqrt(delta))/(2*a)
+            root2 = (b - sqrt(delta))/(2*a)
+            return (root1,root2)
+        if delta == 0 :
+            root = (b/(2*a))
+            return (root,)
+        else:
+            return()
+        pass
+
 
 def integrate(function, lower, upper):
     """Approximates the integral of a fonction between two bounds
-    
+
     Pre: 'function' is a valid Python expression with x as a variable,
          'lower' <= 'upper',
          'function' continuous and integrable between 'lower‘ and 'upper'.
